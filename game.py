010c88db30855162ebfa5acceca1e6d919b10ecb
xiaojiZack <879838984@qq.com>
2020-11-18 23:39:05 +0800

    框架阶段

--- game.py
+++ game.py
@@ -1,7 +1,16 @@
 #游戏主程序
 version = '0.0.1'
 import erajs.api as a
+import page.creat_new_game as creat_new_game
 
+def title():
+    a.page
+    a.mode()
+    a.t('这里是主页面')
+    a.mode('grid',1)
+    a.t()
+    a.b('新的旅程', a.goto, creat_new_game.select_succubus)
+    a.t()
 
 def main():
     a.init()
@@ -10,7 +19,9 @@ def main():
     a.t()
     a.t()
     a.t('version {}'.format(version))
+    a.t()
+    a.b('苏醒',a.goto, title)
 
-    
+
 #---------------
 main()
---
+++ page/creat_new_game.py
@@ -0,0 +1,8 @@
+#创建新存档界面
+import erajs.api as a
+
+def select_succubus():
+    #选择模板
+    a.page()
+    a.t('select')
+    a.b('返回',a.back)
